cb9a03e34c7b9b2105a350647c1342b43e0ff506
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-02-12 10:03:32 +0100

    make all tests pass

--- tdd.string.example/src/string/example/MyStringUtils.java
+++ tdd.string.example/src/string/example/MyStringUtils.java
@@ -5,7 +5,9 @@ public class MyStringUtils {
 	public String leftTrim(String input) {
 		if (input == null || input.isEmpty())
 			return input;
-		return input.substring(1);
+		if (input.charAt(0) == ' ')
+			return input.substring(1);
+		return input;
 	}
 
 }
