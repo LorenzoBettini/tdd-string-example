81bef51d149ac1a75b96c16f84b7334ea3c8dc6e
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-02-12 09:58:55 +0100

    make all tests pass

--- tdd.string.example/src/string/example/MyStringUtils.java
+++ tdd.string.example/src/string/example/MyStringUtils.java
@@ -3,7 +3,9 @@ package string.example;
 public class MyStringUtils {
 
 	public String leftTrim(String input) {
-		return input;
+		if (input == null || input.isEmpty())
+			return input;
+		return input.substring(1);
 	}
 
 }
